e721a7987b954b45ded7c32ca72a104ecdf301c3
deniseleon <kbenisee7@gmail.com>
2025-10-03 15:19:03 -0400

    created 3 classes. one for a for, while, and do while loop.

---
+++ src/main/java/com/pluralsight/WhileLoops.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+        //printing I love Java 5 times
+        int i = 0;
+        while (i <=5 ) {
+            System.out.println("I love Java");
+            i++;
+        }
+
+    }
+}
